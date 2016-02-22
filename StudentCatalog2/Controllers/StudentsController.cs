4f6199ca01a395fee6124799c085f76a52ebc9eb
Christian Kirschberg <Christian Kirschberg>
2016-02-22 10:52:04 +0100

    initial commit

---
+++ StudentCatalog2/Controllers/StudentsController.cs
@@ -0,0 +1,55 @@
+﻿using StudentCatalog2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentCatalog2.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        //action method
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string WannaPlayDad()
+        {
+            return "No, I am coding..";
+        }
+
+        public ActionResult MadView()
+        {
+            //defining Cat property
+            ViewBag.Cat = "Miauu, I am a cat";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            //if no broken rules, defined in student
+            if (ModelState.IsValid)
+            {
+                ApplicationDbContext db = 
+                    new ApplicationDbContext();
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                return View("Thanks");
+            }
+            return View();
+
+        }
+    }
+}
---
+++ StudentCatalog2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentCatalog2.MvcApplication" Language="C#" %>
